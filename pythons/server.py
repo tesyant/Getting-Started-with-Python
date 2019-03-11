99a85b0fb9fa31703a3547d0300879674cbc0369
Tesya Nurintan <tesyasakun@gmail.com>
2019-03-11 20:34:45 +0700

    added files

---
+++ pythons/server.py
@@ -0,0 +1,24 @@
+from flask import (
+    render_template
+    connexion
+)
+
+#create the application instance
+app = connexion.App(__name__, specification_dir='./')
+
+#Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
+#create a URL route in our application for "/"
+@app.route('/')
+def home() :
+
+    # This function just responds to the browser URL localhost:5000/
+    #
+    # :return:    the rendered template 'home.html'
+
+    return render_template('home.html')
+
+#if we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0' port=5000, debug=True)
